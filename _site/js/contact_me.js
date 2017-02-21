28b613592e5408d0014e01b37b7c5c5804cd955a
zorbax <otto94@gmail.com>
2017-02-20 18:18:39 -0600

    update

--- _site/js/contact_me.js
+++ _site/js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "//formspree.io/atgenomics@gmail.com",
                 type: "POST",
                 data: {
                     name: name,
